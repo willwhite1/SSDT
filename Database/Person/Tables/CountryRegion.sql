33791eeb0e67d61d489ecacc2801be7548e1757a
MarcelWouters <marcelwouters@live.nl>
2015-12-09 23:19:08 +0100

    First version

---
+++ Database/Person/Tables/CountryRegion.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [Person].[CountryRegion] (
+    [CountryRegionCode] NVARCHAR (3) NOT NULL,
+    [Name]              [dbo].[Name] NOT NULL,
+    [ModifiedDate]      DATETIME     CONSTRAINT [DF_CountryRegion_ModifiedDate] DEFAULT (getdate()) NOT NULL,
+    CONSTRAINT [PK_CountryRegion_CountryRegionCode] PRIMARY KEY CLUSTERED ([CountryRegionCode] ASC)
+);
+
+
+
---
+++ Deploy/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Dac;
+
+namespace Deploy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DacServices dacServices = new DacServices("server=.");
+            dacServices.ProgressChanged += DacServices_ProgressChanged;
+
+            DacDeployOptions dacDeployOptions = new DacDeployOptions();
+            dacDeployOptions.SqlCommandVariableValues.Add(new KeyValuePair<string, string>("DataFiles", AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\Database\bin\debug"));
+            
+         
+            DacPackage dacPackage = DacPackage.Load(AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\Database\bin\debug\AdventureWorks.dacpac");
+
+            dacServices.Deploy(dacPackage, "AdventureWorksTest", true, dacDeployOptions);
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        private static void DacServices_ProgressChanged(object sender, DacProgressEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
