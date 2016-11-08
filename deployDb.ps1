23b0e24e1b25130135e6cec4f97fcf0df80fd2b1
willwhite1 <willwhite123@yahoo.com>
2016-11-08 16:10:01 -0500

    Update deployDb.ps1

--- deployDb.ps1
+++ deployDb.ps1
@@ -7,12 +7,16 @@ param
 $sqlPackagePath = "C:\Program Files (x86)\Microsoft SQL Server\130\DAC\bin\sqlpackage.exe"
 if (!(Test-Path $sqlPackagePath))
 {
+	"ERROR: Unable to locate sqlpackage.exe at path '$($sqlPackagePath)'" | Add-Content -Path "C:\Builds\Logs\$($SQLInstance)-$($TargetDBName).log"
 	throw "Unable to locate sqlpackage.exe at path '$($sqlPackagePath)'"
 }
 if (!(Test-Path $Dacpac))
 {
+	"ERROR: Unable to locate specified dacpac at path '$($Dacpac)'" | Add-Content -Path "C:\Builds\Logs\$($SQLInstance)-$($TargetDBName).log"
 	throw "Unable to locate specified dacpac at path '$($Dacpac)'"
 }
 
 # run the command
+"VERBOSE: About to execute deployment..." | Add-Content -Path "C:\Builds\Logs\$($SQLInstance)-$($TargetDBName).log"
 & $sqlPackagePath "/Action:Publish" "/SourceFile:`"$($Dacpac)`"" "/TargetServerName:$($SQLInstance)" "/TargetDatabaseName:$($TargetDBName)"
+"VERBOSE: Deployment complete..." | Add-Content -Path "C:\Builds\Logs\$($SQLInstance)-$($TargetDBName).log"
