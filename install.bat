b5b6d98aea52db71995b4dcc13273961fe122f83
willwhite1 <willwhite123@yahoo.com>
2016-11-08 15:57:58 -0500

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,2 +1,2 @@
 REM Call Deployment Util
-c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command "& {'%~dp0\deployDb.ps1' -Dacpac 'C:\Builds\AdventureWorks\AdventureWorks.dacpac' -SQLInstance 'localhost' -TargetDBName 'AdventureWorks'}"
+c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command "& {. %~dp0\deployDb.ps1 -Dacpac 'C:\Builds\AdventureWorks\AdventureWorks.dacpac' -SQLInstance 'localhost' -TargetDBName 'AdventureWorks'}"
